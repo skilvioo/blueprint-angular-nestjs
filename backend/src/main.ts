db96252f9827de1a119fb763162d7a902c2d06e8
Thomas Guigard <thomasguigard@gmail.com>
2020-10-30 13:51:43 +0100

    set .env conf for pgsql ssl

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,12 @@
+
+import * as dotenv from 'dotenv';
+dotenv.config({ path: `${process.cwd()}/.env` });
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.cwd(), "a")
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
